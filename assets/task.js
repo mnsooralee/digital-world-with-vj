fe80ccad3b2ec6fcbb043ae6a6652081022ce651
mnsooralee <mnsooralee@gmail.com>
2025-02-06 21:42:16 +0500

    Add files via upload

---
+++ assets/task.js
@@ -0,0 +1,12 @@
+const sideBtn = document.querySelector("#sidebtn");
+const crossBtn = document.querySelector("#cross");
+const sideBar = document.querySelector(".sidebar");
+
+showSideBar = () => {
+    sideBar.style.display = "flex";
+    console.log("hw");
+}
+hideSideBar = () => {
+    sideBar.style.display = "none";
+    console.log("hw");   
+}
